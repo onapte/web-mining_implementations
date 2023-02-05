f65f8bd9dd31ee0df983c0e892fa57efa242c9a9
Onkar Apte <97520729+onapte@users.noreply.github.com>
2023-02-05 11:02:29 +0530

    Update helper.py

--- index_construction/helper.py
+++ index_construction/helper.py
@@ -1,4 +1,39 @@
+def run_length_encoding(code_string):
+    """
+    Params:
+    code_string: The string on which run length encoding has to be performed
+    
+    Example:
+    code_string: 'aaabbcdaa'
+    run_length_encoding(code_string): 'a3b2c1d1a2'
+    """
+    
+    encoded_string = ''
+    curr_c = code_string[0]
+    counts = 0
+    for c in code_string:
+        if curr_c == '':
+            curr_c = c
+        if c != curr_c:
+            encoded_string += f'{curr_c}{counts}'
+            curr_c = c
+            counts = 1
+        else:
+            counts += 1
+    
+    encoded_string += f'{curr_c}{counts}'
+    return encoded_string
+
 def unary_coding(val):
+    """
+    Params:
+    val: An Integer which has to be converted to unary code
+    
+    Example:
+    val: 5
+    unary_coding(val): 000001
+    """
+    
     unary_string = ''
     for i in range(val):
         unary_string += '0'
